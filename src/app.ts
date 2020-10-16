eb617095b11a8243b0e29ed8b3bee0e8621f26b6
Francis Lagares <francis.lagares@gmail.com>
2020-10-16 18:12:52 +0200

    feat: create re-usable validation functionality

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,47 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLenght?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLenght != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLenght;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // Auto bind decorators
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -17,7 +61,7 @@ class ProjectInput {
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
-  descriptionElement: HTMLTextAreaElement;
+  descInputElement: HTMLTextAreaElement;
   peopleInputElement: HTMLInputElement;
 
   constructor() {
@@ -35,7 +79,7 @@ class ProjectInput {
     this.titleInputElement = this.element.querySelector(
       '#title'
     )! as HTMLInputElement;
-    this.descriptionElement = this.element.querySelector(
+    this.descInputElement = this.element.querySelector(
       '#description'
     )! as HTMLTextAreaElement;
     this.peopleInputElement = this.element.querySelector(
@@ -46,10 +90,54 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const inputTitle = this.titleInputElement.value;
+    const inputDescription = this.descInputElement.value;
+    const inputPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: inputTitle,
+      required: true,
+    };
+    const descValidatable: Validatable = {
+      value: inputDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +inputPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!');
+    } else {
+      return [inputTitle, inputDescription, +inputPeople]; // By default TypeScript extracts values as strings, so we need to convert number values with +
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    // TypeScript tuples are arrays so we can check them with Array.isArray()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
